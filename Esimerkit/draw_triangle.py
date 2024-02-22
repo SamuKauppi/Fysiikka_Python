e6f67dddc1fabef0614175d41ff51fbaa2f22e1e
HP <samu.kauppi@metropolia.fi>
2024-02-22 11:49:52 +0200

    add triangle movement

--- Esimerkit/draw_triangle.py
+++ Esimerkit/draw_triangle.py
@@ -1,15 +1,46 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-
+from math import sin, cos, radians
 
 # Coordinates of our triangle
-pts = np.array([[2,2], [2,4], [4,2]])
-p = Polygon(pts)
-Polygon.set_fill(p, False)
+triangle = [[0.0,0.0], [0.0,2.0], [2.0,0.0]]
+
+cmx = 1/3 * (triangle[0][0] + triangle[1][0] + triangle[2][0])
+cmy = 1/3 * (triangle[0][1] + triangle[1][1] + triangle[2][1])
+
+cms = [
+    [cmx, cmy]
+]
+
+v0 = 10.0   # 10 m/s
+kulma = radians(45)
+vx = v0 * cos(kulma)
+vy = v0 * sin(kulma)
+
+t = 0.0     # time
+dt = 0.1    # delta time
+
 ax = plt.gca()
-ax.add_patch(p)
-ax.set_xlim(1,7)
-ax.set_ylim(1,8)
+while t < 2.0:
+    t += dt
+    cmxl = cms[-1][0] + vx * dt
+    cmyl = cms[-1][1] + vy * dt
+
+    cms.append([cmxl, cmyl])
+
+    new_triangle = [
+        [triangle[0][0] + cmxl, triangle[0][1] + cmxl],
+        [triangle[1][0] + cmxl, triangle[1][1] + cmxl],
+        [triangle[2][0] + cmxl, triangle[2][1] + cmxl]
+    ]
+
+    p = Polygon(new_triangle)
+    Polygon.set_fill(p, False)
+    ax.add_patch(p)
+
+# Determine plot area size
+ax.set_xlim(-5,20)
+ax.set_ylim(-5,20)
+
+# show plot
 plt.show()
