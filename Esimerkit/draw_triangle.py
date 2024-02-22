c73679711b986f819c4221b7573890a09413f9a6
HP <samu.kauppi@metropolia.fi>
2024-02-22 12:32:35 +0200

    fix triangle movement

--- Esimerkit/draw_triangle.py
+++ Esimerkit/draw_triangle.py
@@ -3,44 +3,60 @@ from matplotlib.patches import Polygon
 from math import sin, cos, radians
 
 # Coordinates of our triangle
-triangle = [[0.0,0.0], [0.0,2.0], [2.0,0.0]]
+triangle = [[2.0, 0.0], [-1.0, 1.0], [-1.0, -1.0]]
 
 cmx = 1/3 * (triangle[0][0] + triangle[1][0] + triangle[2][0])
 cmy = 1/3 * (triangle[0][1] + triangle[1][1] + triangle[2][1])
 
 cms = [
     [cmx, cmy]
-]
+    ]
 
-v0 = 10.0   # 10 m/s
-kulma = radians(45)
+new_triangle = [
+        [triangle[0][0] + cms[-1][0], triangle[0][1] + cms[-1][1]],
+        [triangle[1][0] + cms[-1][0], triangle[1][1] + cms[-1][1]],
+        [triangle[2][0] + cms[-1][0], triangle[2][1] + cms[-1][1]]
+    ]
+
+v0 = 12.0   # 10 m/s
+kulma = radians(65)
 vx = v0 * cos(kulma)
 vy = v0 * sin(kulma)
 
+g = 9.81    # gravity
 t = 0.0     # time
 dt = 0.1    # delta time
 
 ax = plt.gca()
-while t < 2.0:
-    t += dt
-    cmxl = cms[-1][0] + vx * dt
-    cmyl = cms[-1][1] + vy * dt
+p = Polygon(new_triangle)
+Polygon.set_fill(p, False)
+ax.add_patch(p)
 
-    cms.append([cmxl, cmyl])
+while t < 2.0:
+
+    cmx = cms[-1][0] + vx * dt
+    vyl = vy - g*dt
+    cmy = cms[-1][1] + vyl * dt
+
+    cms.append([cmx, cmy])
 
     new_triangle = [
-        [triangle[0][0] + cmxl, triangle[0][1] + cmxl],
-        [triangle[1][0] + cmxl, triangle[1][1] + cmxl],
-        [triangle[2][0] + cmxl, triangle[2][1] + cmxl]
+        [triangle[0][0] + cms[-1][0], triangle[0][1] + cms[-1][1]],
+        [triangle[1][0] + cms[-1][0], triangle[1][1] + cms[-1][1]],
+        [triangle[2][0] + cms[-1][0], triangle[2][1] + cms[-1][1]]
     ]
 
     p = Polygon(new_triangle)
     Polygon.set_fill(p, False)
     ax.add_patch(p)
 
+    t += dt
+    vy = vyl
+
+
 # Determine plot area size
-ax.set_xlim(-5,20)
-ax.set_ylim(-5,20)
+ax.set_xlim(-5,25)
+ax.set_ylim(-5,25)
 
 # show plot
 plt.show()
