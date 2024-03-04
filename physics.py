f87714df593d8ab313b91538e3a7a91905edb46c
HP <samu.kauppi@metropolia.fi>
2024-03-04 14:31:51 +0200

    add todo

--- physics.py
+++ physics.py
@@ -6,8 +6,8 @@ from math import sin, cos, radians
 xoffset = 2
 yoffset = 30
 
-# Starting coordinates of our triangle
-triangle = [[2.0, 2.0], [-2.0, 2.0], [-2.0, -2.0], [2.0, -2.0]]
+# First objectt dimensions
+object1 = [[0.5, 2.0], [-2.0, 2.0], [-2.0, -2.0], [0.5, -2.0], [3.0, 0.0]]
 
 # Movement
 v0 = 17.0   # 10 m/s
@@ -20,9 +20,10 @@ dt = 0.2    # delta time
 
 # Rotation
 wv = -100           # radians(deg)/s
-dr = wv * dt
+dr = wv * dt        # radians
 
-obj1 = Object(xoffset, yoffset, v0, angle, wv, triangle, dt)
+# determine object
+obj1 = Object(xoffset, yoffset, v0, angle, wv, object1, dt)
 
 # polygon
 polygons = []
@@ -32,19 +33,30 @@ p = Polygon(obj1.object)
 Polygon.set_fill(p, False)
 polygons.append(p)
 
-while t < 3.5:
+while t < 10:
 
     # Center of mass position
     cm = obj1.get_new_cm()
 
-    new_triangle = obj1.get_new_rotation()
+    # Get rotated object
+    new_object = obj1.get_new_rotation()
 
-    new_triangle = [[x + cm[0], y + cm[1]] for x, y in new_triangle]
+    # Move rotated object to center of mass
+    new_object = [[x + cm[0], y + cm[1]] for x, y in new_object]
 
-    poly = Polygon(new_triangle)
+    # TODO: 
+    # - ground check
+    # - nopeuden muutos
+    # - kulmanopeuden muutos
+    # - collision check
+    # 
+
+    # create a polygon and add it to list
+    poly = Polygon(new_object)
     Polygon.set_fill(poly, False)
     polygons.append(poly)
 
+    # increment time
     t += dt
 
 
@@ -54,9 +66,13 @@ for p in polygons:
     ax.add_patch(p)
 
 # Determine plot area size
-ax.set_xlim(-5,50)
+ax.set_xlim(-5,110)
 ax.set_ylim(-5,50)
 ax.set_aspect('equal')
 
+# draw ground
+x = [-10, 120]
+y = [-0.1, 0.1]
+plt.plot(x, y, marker = 'o', color = 'r')
 # show plot
 plt.show()
